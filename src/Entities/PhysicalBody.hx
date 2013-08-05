8dd23dcb546487d0b89d666c53f6550c6148d547
Shane <shane.king.2008@gmail.com>
2013-08-05 07:37:08 -0500

    Removed unnecessary function override.

--- src/Entities/PhysicalBody.hx
+++ src/Entities/PhysicalBody.hx
@@ -7,11 +7,6 @@ class PhysicalBody extends Entity
 {
 	private var body:Body;
 
-    public function new(x:Int, y:Int, r = 5, mass = 0.1)
-    {
-        super(x, y);
-    }
-
     public function getBody(){
     	return body;
     }
